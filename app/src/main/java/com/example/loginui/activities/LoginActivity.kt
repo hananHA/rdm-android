8b2812eb2d7d4334d214f7958a1158ddeeef6c2f
Emad <emad.batwa@gmail.com>
2020-02-02 21:34:11 +0300

    delete Retrofit lib

--- app/src/main/java/com/example/loginui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/loginui/activities/LoginActivity.kt
@@ -3,8 +3,13 @@ package com.example.loginui.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.loginui.R
 import kotlinx.android.synthetic.main.activity_login.*
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -40,7 +45,32 @@ class LoginActivity : AppCompatActivity() {
             }
 
 
+// Instantiate the RequestQueue.
+            val queue = Volley.newRequestQueue(this)
+            val url = "http://testtamayoz.tamayyozz.net/api/login"
+
+// Request a string response from the provided URL.
+            val stringRequest = StringRequest(Request.Method.POST, url,
+                Response.Listener<String> { response ->
+                    // Display the first 500 characters of the response string.
+                    print("Ok")
+                    print(response.substring(0, 10))
+                },
+                Response.ErrorListener { error ->
+
+                    println("Error No")
+                    println(error.toString())
+                })
+
+// Add the request to the RequestQueue.
+            queue.add(stringRequest)
+
+
         }
+
+
+
+
 
         registerText.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
--- app/src/main/java/com/example/loginui/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/loginui/activities/RegisterActivity.kt
@@ -4,15 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.loginui.R
-import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_register.*
-
 
 class RegisterActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+
 
 
 
