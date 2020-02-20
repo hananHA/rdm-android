22ba7d10a5fa5a4e69e31b3391a45eb5dd82dc1c
Emad <emad.batwa@gmail.com>
2020-02-20 21:42:27 +0300

    fix bugs !

--- app/src/main/java/com/example/rdm/activities/TicketListActivity.java
+++ app/src/main/java/com/example/rdm/activities/TicketListActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.rdm.Model.App;
 import com.example.rdm.Model.TicketList;
@@ -22,6 +24,7 @@ public class TicketListActivity extends AppCompatActivity {
 
     public static List<JSONObject> ticketArrayList = new ArrayList<>();
     RecyclerView recyclerView;
+    TextView hasText;
     //a list to store all the products
     List<TicketList> ticketListClass;
 
@@ -34,6 +37,8 @@ public class TicketListActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         ticketListClass = new ArrayList<>();
+        hasText = findViewById(R.id.hasTicket);
+        hasText.setVisibility(View.INVISIBLE);
 
 
         loadListTicket();
@@ -42,43 +47,51 @@ public class TicketListActivity extends AppCompatActivity {
 
     public void loadListTicket() {
 
+
         try {
 
             JSONArray jsonarray = new JSONArray(App.listTicketResponse);
-            int i;
-            JSONObject json_data;
-            JSONObject ticket = null;
-            for (i = 0; i < jsonarray.length(); i++) {
-                json_data = jsonarray.getJSONObject(i);
-                ticket = (JSONObject) json_data.get("ticket");
-                ticketArrayList.add(i, ticket);
-            }
+            if (jsonarray.length() == 0) {
+                hasText.setVisibility(View.VISIBLE);
+
+            } else {
 
 
-            for (i = 0; i < ticketArrayList.size(); i++) {
-                ticketListClass.add(
-                        new TicketList(
-                                ticketArrayList.get(i).getInt("id"),
-                                ticketArrayList.get(i).getString("description"),
-                                ticketArrayList.get(i).getString("status"),
-                                ticketArrayList.get(i).getString("classification")
-                        ));
+                int i;
+                JSONObject json_data;
+                JSONObject ticket = null;
+                for (i = 0; i < jsonarray.length(); i++) {
+                    json_data = jsonarray.getJSONObject(i);
+                    ticket = (JSONObject) json_data.get("ticket");
+                    ticketArrayList.add(i, ticket);
+                }
 
-            }
+
+                for (i = 0; i < ticketArrayList.size(); i++) {
+                    ticketListClass.add(
+                            new TicketList(
+                                    ticketArrayList.get(i).getInt("id"),
+                                    ticketArrayList.get(i).getString("description"),
+                                    ticketArrayList.get(i).getString("status"),
+                                    ticketArrayList.get(i).getString("classification")
+                            ));
+
+                }
 //
-            //creating recyclerview adapter
-            TicketListAdapter adapter = new TicketListAdapter(this, ticketListClass);
+                //creating recyclerview adapter
+                TicketListAdapter adapter = new TicketListAdapter(this, ticketListClass);
 
-            //setting adapter to recyclerview
-            recyclerView.setAdapter(adapter);
-
+                //setting adapter to recyclerview
+                recyclerView.setAdapter(adapter);
+            }
 
         } catch (Exception e) {
             Log.e("error load list ticket", e.getMessage());
 
         }
     }
-
-
 }
+
+
+
 
